3883ebf3819c805c8c0944d496c53e64629c50e5
yashvixshah <156948769+yashvixshah@users.noreply.github.com>
2025-01-08 16:38:46 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=12222 
+let accountEmail="shahyashvi1103@gmail.com"
+var accountPassword="12345678"
+accountCity="Ahmedabad"
+let accountState
+/* prefer not to use var due to block scope problem */
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
