e2827cf2761d20740727f104017ae1a622ff64e4
yashvixshah <156948769+yashvixshah@users.noreply.github.com>
2025-01-08 18:18:03 +0000

    Comparisons and Datatypes Summary

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+/*console.log(2 > 1);
+console.log(2 == 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 <= 1)*/
+
+//comparison datatypes should be same otherwise not predictable results
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) //gives unpredictable results
+
+// triple === checks datatypes and value both
+
+console.log("2" === "2")
---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,32 @@
+// ***** Primitive data types *****
+
+//CALL BY VALUE ONLY
+// 7 types: String, Number, Boolean, Null, undefined, Symbol
+
+const score=55;
+let name="yashvi"
+let age=18
+
+const id= Symbol('123');
+const id2= Symbol('123');
+
+console.log(id === id2) //not same will be unique
+
+//it is dynamically typed language as you dont need to mention specific type
+
+
+// ***** REFERENCE TYPES *****
+
+// ARRAY, OBJECTS AND FUNCTIONS
+const heroes = ["yashvi","deep","rohan","prasham"]; //array
+
+let myObj=          //object stored in variable
+{
+    name: "yashvi", //objects
+    age: 22
+}
+
+//function example
+const myFunction=function(){
+    console.log("Basic function");
+}
