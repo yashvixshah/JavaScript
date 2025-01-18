c4bef4eb950b52a30be258f243794c28ee73c4d9
yashvixshah <156948769+yashvixshah@users.noreply.github.com>
2025-01-18 14:42:06 +0000

    if-else basics

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+//if statement
+// ==,!=,<=,>=,<,>,===(STRICT EQUAL TO FOR DATATYPE CHECK)
+
+// let isUserLoggedIn=true
+
+// if(isUserLoggedIn)
+// {
+//     console.log("User is logged in")
+// }
+// else{
+//     console.log("User logged out")
+// }
+
+
+// const balance=1000
+// if(balance > 500) console.log("Great")  //short hand notation
+
+const isUserLoggedIn=true
+const debitCard=true
+
+if(isUserLoggedIn && debitCard)
+{
+    console.log("Allowed to buy")
+}
