d6e1df65e63716aa50c90ef86bd8a277041b8d8a
yashvixshah <156948769+yashvixshah@users.noreply.github.com>
2025-01-19 08:49:27 +0000

    for in loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,20 @@
+// //for in loop
+// const myObject=
+// {
+//     js: "javascript",
+//     cpp: "c++",
+//     py: "python"
+// }
+
+// for(const i in myObject)
+// {
+//     console.log(myObject[i])
+// }
+
+const languages=["c","c++","java","python"]
+for(const l in languages)
+{
+    //console.log(l)   //printing l would print just keys
+    console.log(languages[l])
+}
+
