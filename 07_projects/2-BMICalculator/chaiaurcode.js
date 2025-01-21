ce18b2de2900e335b785de2e5392c5330e88be8e
yashvixshah <156948769+yashvixshah@users.noreply.github.com>
2025-01-21 13:34:26 +0000

    project 2

---
+++ 07_projects/2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form")
+
+//submit eveny
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()  //pause default submit action
+  const height=parseInt(document.querySelector("#height").value)
+  const weight=parseInt(document.querySelector("#weight").value)
+  const results=document.querySelector('#results')
+
+  if(height==="" || height<0 || isNaN(height))
+  {
+    results.innerHTML="Please give valid height"
+  }
+
+  else if(weight==="" || weight<0 || isNaN(weight))
+  {
+    results.innerHTML="Please give valid weight"
+  }
+
+  else{
+    const bmi=(weight/((height*height)/10000)).toFixed(2)
+
+    results.innerHTML=bmi
+  }
+})
