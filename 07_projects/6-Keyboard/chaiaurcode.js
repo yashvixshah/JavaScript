2285c6e7f1d6d73ba3a461f08ecdd2f5d99ac8ff
yashvixshah <156948769+yashvixshah@users.noreply.github.com>
2025-04-03 17:45:05 +0000

    keyboard press project

---
+++ 07_projects/6-Keyboard/chaiaurcode.js
@@ -0,0 +1,15 @@
+const insert=document.querySelector("insert")
+window.addEventListener('keypress',(e)=>{
+    insert.innerHTML=`<div><table>
+        <tr>
+            <th>Key</th>
+            <th>Key Code</th>
+            <th>Code</th>
+        </tr>
+        <tr>
+            <td>${e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code}</td>
+        </tr>
+    </table><div>`
+})
